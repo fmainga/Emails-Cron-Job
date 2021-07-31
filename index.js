1dc1bf9417186ebac334f0214f0085d418e2eb18
Fred Mainga <maingafred@yahoo.com>
2021-07-31 21:18:44 +0300

    Added hotfix

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ let transporter = nodemailer.createTransport({
 });
 
 // Sending emails every Monday.
+//more help on crontab.tech
 cron.schedule('0 0 * * 1', function() {
     console.log('--------Running Email Cron Job-------------');
     console.log('Sending email...');
